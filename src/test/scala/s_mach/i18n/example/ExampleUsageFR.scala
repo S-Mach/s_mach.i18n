7461c8529260a39be5466c9041c060cdeda3b720
Lance Gatlin <lance.gatlin@gmail.com>
2016-05-01 10:56:00 -0400

    work on readme [skip ci]

--- src/test/scala/s_mach/i18n/example/ExampleUsageFR.scala
+++ src/test/scala/s_mach/i18n/example/ExampleUsageFR.scala
@@ -15,10 +15,10 @@ object ExampleUsageFR {
   // Using i18ncfg, resolve the message key to a message format and interpolate the arguments
   // Note: the Int argument is automatically converted to I18NString by invoking the
   // I18N[Int] type-class
-  val test_message_lookup : I18NString = m_test(1,"test".asI18N)
+  val test_message_lookup : I18NString = m_test(100000,"test".asI18N)
 
   // Note: the Double argument is automatically converted to I18NString by invoking the
   // I18N[Double] type-class
-  val test_i18n_str : I18NString = i18n"nous allons interpoler certains arguments $test_message_lookup ${25.0}"
+  val test_i18n_str : I18NString = i18n"nous allons interpoler certains arguments $test_message_lookup ${10000.999}"
 
 }
