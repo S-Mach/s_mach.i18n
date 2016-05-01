86f0ce75b2c3b2b0b4fccd50241ea26670f71ae8
Lance Gatlin <lance.gatlin@gmail.com>
2016-05-01 10:41:26 -0400

    work on readme [skip ci]

--- src/test/scala/s_mach/i18n/example/ExampleUsageEnglish.scala
+++ src/test/scala/s_mach/i18n/example/ExampleUsageEnglish.scala
@@ -11,15 +11,15 @@ implicit val i18ncfg = I18NConfig(Locale.ENGLISH)
 
 // Create a message with the key m_test that accepts 2 arguments, an Int and an I18NString
 // Note: this is sugar for Message2[Int,I18NString]('m_test)
-val m_test = 'm_test.m[Int,I18NString]
+val m_test : Message2[Int,I18NString] = 'm_test.m[Int,I18NString]
 
 // Using i18ncfg, resolve the message key to a message format and interpolate the arguments
 // Note: the Int argument is automatically converted to I18NString by invoking the
 // I18N[Int] type-class
-val test_message_lookup = m_test(1,"test".asI18N)
+val test_message_lookup : I18NString = m_test(1,"test".asI18N)
 
 // Note: the Double argument is automatically converted to I18NString by invoking the
 // I18N[Double] type-class
-val test_i18n_str = i18n"interpolate some arguments $test_message_lookup ${25.0}"
+val test_i18n_str : I18NString = i18n"interpolate some arguments $test_message_lookup ${25.0}"
 
 }
